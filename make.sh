21b2314a660bc7098a0069a5f2ba6988ae09d2f1
ImPeekaboo <apapkh@gmail.com>
2024-12-04 01:43:17 +0800

    add android arm64 build

--- make.sh
+++ make.sh
@@ -4,12 +4,17 @@ if [[ "$1" = "t" ]]; then
 elif [[ -n "$1" ]]; then
     CGO_ENABLED=0 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
     GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
+    GOOS=android GOARCH=arm64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)" -o ytarchive_android_arm64
 else
     CGO_ENABLED=0 go build
     GOOS=windows GOARCH=amd64 go build
+    GOOS=android GOARCH=arm64 go build -o ytarchive_android_arm64
 fi
 
 zip ytarchive_linux_amd64.zip ytarchive
 zip ytarchive_windows_amd64.zip ytarchive.exe
+rm ytarchive
+mv ytarchive_android_arm64 ytarchive
+zip ytarchive_android_arm64.zip ytarchive
 
-sha256sum ytarchive_linux_amd64.zip ytarchive_windows_amd64.zip > SHA2-256SUMS
+sha256sum ytarchive_linux_amd64.zip ytarchive_windows_amd64.zip ytarchive_android_arm64.zip > SHA2-256SUMS
